3bedebbf4e911fe7fd118ca6cebe943c32ac4018
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-04-22 21:56:26 -0300

    feat: integração dos padrões e execução do sistema no Main

--- src/main/com/rh/Main.java
+++ src/main/com/rh/Main.java
@@ -1,7 +1,30 @@
 package com.rh;
 
+import com.rh.abstractfactory.*;
+import com.rh.bridge.*;
+import com.rh.factorymethod.Funcionario;
+import com.rh.singleton.RHManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sistema de RH iniciado...");
+
+        FuncionarioFactory cltFactory = new CLTFactory();
+        FuncionarioFactory pjFactory = new PJFactory();
+
+        Funcionario f1 = cltFactory.criarFuncionario("João");
+        Funcionario f2 = pjFactory.criarFuncionario("Maria");
+
+        Beneficio vt = new ValeTransporte();
+        Beneficio ps = new PlanoSaude();
+
+        FuncionarioBridge fb1 = new FuncionarioBridge(f1, vt);
+        FuncionarioBridge fb2 = new FuncionarioBridge(f2, ps);
+
+        RHManager manager = RHManager.getInstance();
+        manager.adicionarFuncionario(f1);
+        manager.adicionarFuncionario(f2);
+
+        fb1.exibirDetalhes();
+        fb2.exibirDetalhes();
     }
 }
